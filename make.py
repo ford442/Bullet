67474d1afbe58f5583869500fc8c65d246fdb63c
Noah Cohn <noahc42@gmail.com>
2021-08-31 13:20:35 -0700

    Update make.py

--- make.py
+++ make.py
@@ -6,7 +6,6 @@ from subprocess import Popen, PIPE, STDOUT
 # Definitions
 
 INCLUDES = ['btBulletDynamicsCommon.h', os.path.join('BulletCollision', 'CollisionShapes', 'btHeightfieldTerrainShape.h'), os.path.join('BulletCollision', 'CollisionDispatch', 'btGhostObject.h'), os.path.join('BulletDynamics', 'Character', 'btKinematicCharacterController.h')]
-# Startup
 
 
 
